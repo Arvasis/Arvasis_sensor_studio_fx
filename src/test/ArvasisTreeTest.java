dac1813f485bafe15134b1448f51d9be90f6d627
Melek <melekozyavuz@arvasis.com>
2021-02-05 13:56:46 +0300

    Added condition to Tree

---
+++ src/test/ArvasisTreeTest.java
@@ -0,0 +1,42 @@
+package test;
+
+import java.awt.image.BufferedImage;
+
+import arvasis.camera.Camera;
+import arvasis.camera.VirtualCamera;
+import arvasis.drawing.GraphicsIO;
+import arvasis.sensor.studio.tree.ArvasisTree;
+import arvasis.sensor.studio.tree.TreeNode;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TreeItem;
+import javafx.stage.Stage;
+
+public class ArvasisTreeTest extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Camera cam = new VirtualCamera();
+		BufferedImage image = GraphicsIO
+				.readBufferedImage("C:\\Users\\Arvasis\\Documents\\KT\\img\\642969956309746.jpg");
+		TreeNode root = new TreeNode(cam.toString(), image);
+		
+		ArvasisTree tree = new ArvasisTree(root);
+		BufferedImage image1 = GraphicsIO.applyCanny(image);
+		BufferedImage image2 = GraphicsIO.applyDilation(image1, 3);
+		BufferedImage image3 = GraphicsIO.applyErosion(image, 2);
+
+		tree.addChild(new TreeNode("Canny", image1));
+		tree.addChild(new TreeNode("Dilation", image2));
+	//	tree.addChild(new TreeNode("Erosion", image3));
+		tree.getRoot().getChildren().add(0,new TreeItem<TreeNode>(new TreeNode("Erosion", image3)));
+
+		Scene scene = new Scene(tree);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
